971014f99a7b106581fa0736a11302594df8bb1b
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-05-31 02:42:22 -0400

    cointainer update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import * as dat from "lil-gui"
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js"
 
 /**
  * Base
@@ -82,12 +83,54 @@ const particlesMaterial = new THREE.PointsMaterial()
   (particlesMaterial.sizeAttenuation = true),
   (particlesMaterial.color = new THREE.Color("#ffffff"))
 
-// add box
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const boxMaterial = new THREE.MeshBasicMaterial({ color: "#FFC0CB" })
-const box = new THREE.Mesh(boxGeometry, boxMaterial)
-box.position.set(0, 0, 0)
-scene.add(box)
+// add Model
+let mixer = null
+
+const loader = new OBJLoader()
+loader.load(
+  "/models/cointainer.obj",
+  function (object) {
+    scene.add(object)
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded")
+  },
+  function (error) {
+    console.log("An error happened")
+  }
+)
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(50, 50),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
+)
+floor.receiveShadow = true
+floor.rotation.x = -Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = -7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = -9
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
@@ -192,9 +235,16 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
+
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
 
   // Update particles
   particles.rotation.y = elapsedTime * 0.1
